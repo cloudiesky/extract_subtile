799e834dd0675063a734dbb0bccea7a5643779b8
cloudiesky <cloudieskyx@gmail.com>
2024-01-21 13:07:52 +0800

    Update ff.py

--- ff.py
+++ ff.py
@@ -18,28 +18,61 @@ def extract_subtitles(video_file):
     output = result.stderr
     print(output)
 
-    map_lines = re.findall(r'Stream #0:([0-9]+)\((.*)\): Subtitle: subrip (.*?)\n', output)
+    pattern = r'Stream #(\d+):(\d+)\((\w+)\): Subtitle: subrip.*?title\s+: (.*?)\s+'
+    map_lines_0 = re.findall(pattern, output, re.DOTALL)
+
+    pattern = r'Stream #(\d+):(\d+)\((\w+)\): Subtitle: subrip(.*?)\n'
+    map_lines_1 = re.findall(pattern, output)
+    if map_lines_0 != []:
+        map_lines = map_lines_0
+    elif map_lines_1 != []:
+        map_lines = map_lines_1
+    else:
+        print("no match lines")
     print(map_lines)
 
-    first_stream_id = int(map_lines[0][0]) 
+    first_stream_id = int(map_lines[0][1]) 
 
-    for stream_id, lang, unkown in map_lines:
+    for vid, stream_id, lang, title in map_lines:
+        if map_lines == map_lines_1:
+            title = "dump"
+        if lang in ["eng", "chi"]:  # 只有当lang为"eng"或"chi"时才执行下面的代码
+             # 根据 unkown 内容进一步确定 lang 的取值
+            if lang == "chi":
+                #if title in ['Simplified',"简体中文"] :
+                if "Simplified" in title or "简" in title:
+                    lang = "chs"
+                else:
+                    lang = "cht"
 
-        # 根据第一个字幕流的stream_id调整后续的stream_id
-        adjusted_stream_id = int(stream_id) - first_stream_id  
+            # 根据第一个字幕流的stream_id调整后续的stream_id
+            adjusted_stream_id = int(stream_id) - first_stream_id  
 
-        # 设置输出字幕文件的名称
-        output_subtitle_file = f"{os.path.splitext(video_file)[0]}.{lang}.srt"
-        ass = f"{os.path.splitext(video_file)[0]}.{lang}.ass"
+            # 设置输出字幕文件的名称
+            output_subtitle_file = f"{os.path.splitext(video_file)[0]}.{lang}.srt"
+            ass = f"{os.path.splitext(video_file)[0]}.{lang}.ass"
 
-        # 提取字幕
-        subprocess.run([ffmpeg, "-i", video_file, "-map", f"0:s:{adjusted_stream_id}", "-c:s", "srt", "-n", output_subtitle_file, "-loglevel", "quiet"])
+            print(f"Extracting subtitle stream {stream_id} to {ass}")
+
+            # 提取字幕
+            #result = subprocess.run([ffmpeg, "-i", video_file, "-map", f"0:s:{adjusted_stream_id}", "-c:s", "srt", "-n", output_subtitle_file, "-loglevel", "quiet"])
+            result = subprocess.run([ffmpeg, "-i", video_file, "-map", f"0:s:{adjusted_stream_id}", "-c:s", "ass", "-n", ass, "-loglevel", "quiet"])
         
-        subprocess.run([ffmpeg, "-i", output_subtitle_file, "-n", ass])
+            #subprocess.run([ffmpeg, "-i", output_subtitle_file, "-n", ass])
 
-        # 提示用户
-        print(f"Extracted subtitle stream {stream_id} to {output_subtitle_file}")
+            # 提示用户
+            print(f"Extracted subtitle stream {stream_id} to {ass}")
+
+def find_and_extract_subtitles(directory):
+    if os.path.isdir(directory):  # 检查输入路径是否为目录
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.endswith((".mp4", ".mkv", ".avi")):  # 检查文件扩展名
+                    video_file = os.path.join(root, file)  # 获取视频文件的完整路径
+                    extract_subtitles(video_file)  # 调用extract_subtitles函数提取字幕
+    else:
+        print(f"{directory} is not a valid directory.")
 
 if __name__ == "__main__":
-    video_file = sys.argv[1]
-    extract_subtitles(video_file)
+    video_path = sys.argv[1]
+    find_and_extract_subtitles(video_path)
